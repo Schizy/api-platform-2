6ddac24ccaf280588acfca4a9fdb39d60840085c
Schizy <ali.mechichi@gmail.com>
2024-09-10 19:14:30 +0200

    Chapitre 5

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use ApiPlatform\Api\IriConverterInterface;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,12 +11,14 @@ use Symfony\Component\Security\Http\Attribute\CurrentUser;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: ['POST'])]
-    public function login(#[CurrentUser] User $user = null): Response
+    public function login(IriConverterInterface $iriConverter, #[CurrentUser] User $user = null): Response
     {
         if (!$user) {
             return $this->json(['error' => 'Did you forget the Content-Type: "application/json" ?'], 401);
         }
 
-        return $this->json(['user' => $user->getId()]);
+        return new Response(null, Response::HTTP_NO_CONTENT, [
+            'Location' => $iriConverter->getIriFromResource($user)
+        ]);
     }
 }
