64351409e3f7ba05e456484cf4ff352d6d5feb5a
Schizy <ali.mechichi@gmail.com>
2024-09-12 19:24:36 +0200

    Chapitre 17

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -3,7 +3,9 @@
 namespace App\Tests\Functional;
 
 use App\Factory\DragonTreasureFactory;
+use App\Factory\UserFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\Response;
 use Zenstruck\Browser\Test\HasBrowser;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
@@ -28,5 +30,28 @@ class DragonTreasureResourceTest extends KernelTestCase
         $this->assertSame(array_keys($json->decoded()['hydra:member'][0]), [
             '@id', '@type', 'name', 'description', 'value', 'coolFactor', 'owner', 'shortDescription', 'plunderedAtAgo',
         ]);
+    }
+
+    public function testPostToCreateTreasure(): void
+    {
+        $user = UserFactory::createOne();
+
+        $this->browser()
+            ->actingAs($user)
+            ->post('/api/treasures', [
+                'json' => []
+            ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->post('/api/treasures', [
+                'json' => [
+                    'name' => 'A shiny thing',
+                    'description' => 'It sparkles when I wave it in the air.',
+                    'value' => 1000,
+                    'coolFactor' => 5,
+                    'owner' => '/api/users/' . $user->getId(),
+                ],
+            ])
+            ->assertStatus(Response::HTTP_CREATED)
+            ->assertJsonMatches('name', 'A shiny thing');
     }
 }
